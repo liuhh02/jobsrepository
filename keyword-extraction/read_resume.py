326f85d738f84d825db96e565c9e0f78df6f58dd
Borna Sadeghi <sadeghiborna@gmail.com>
2020-05-16 15:38:51 -0400

    init commit for keyword extraction folder

---
+++ keyword-extraction/read_resume.py
@@ -0,0 +1,34 @@
+def eliminate_bad_chars (string):
+    '''
+    Replaces unwanted characters with a space.
+
+    Why a space? imagine you have words seperated by a '/'. This prevents the words from being concatenated end to end.
+
+    The '-' symbols are kept for a similar reason. Conjunction words with '-' typically have different meanings from their components.
+    '''
+    bad_chars = "!@#$%^&*()[]{};:,./<>?\|`~-=_•'"
+    for char in bad_chars:
+        string = string.replace(char, " ")
+    return string
+
+def clean(filename):
+    '''
+    Clean a text file of certain characters and return a set of the words.
+
+    Returns: list of words
+    '''
+    with open(filename) as f:
+        lines = f.readlines()
+
+    lines_new = []
+
+    for line in lines:
+        line = eliminate_bad_chars(line.lower()).strip()
+
+        for word in line.split(" "):
+            if word != '':
+                lines_new.append(word)
+
+    return list(set(lines_new))
+
+print(clean("resume.txt"))
