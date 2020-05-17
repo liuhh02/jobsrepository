974612a5dc294c55808e3d5fe4ce6c0ecd21386b
Borna Sadeghi <sadeghiborna@gmail.com>
2020-05-17 03:34:38 -0400

    working towards authentication

--- jobs_connect.py
+++ jobs_connect.py
@@ -1,25 +1,147 @@
-from google.cloud import talent_v4beta1
+import os
 
-GOOGLE_APPLICATION_CREDENTIALS = {
-  "type": "service_account",
-  "project_id": "resumatch-277502",
-  "private_key_id": "58e1a4618baa5e6be2c89cdfc9ca48dcea25ca8e",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDBz7LG2dMiTpbJ\ncbNjzTjdLAM2Pg/GeAM9/bF9zBv7qdPMtrW6op5+DHw0IHhbwOVNoowAkOonPYIv\nB9HfbECoM+Tzf7KPYLa07O4hwL480eRvDK4whaiLZRLZhL2OPceo5ax1eTdhJDjg\nyMa9FZkEac9BnRZeyrA+yOh1ERJymvvk+/MZ0rPZyY/WeB3QM1yJ9YIS84GhKKl/\nkzJA7sU+vLxLpscXqJX1g0HW9x06Rti74Z0w56mzsINJMNKIBLFm66Mkuqm7/Xz+\nC7+ZbESXePTL8rDt0pxUwUiTVztjY3blB8rC7r5PVB5j7/v42vXDOyL54MR4OVrU\nN0SHtyyvAgMBAAECggEAUN2I0ehT7FYPGykKOOOJTNsO0gSOD3jGnYhrFLpjmJsw\n6JL5e1BbwesSras4nMv8wOpH8PqGAG5tEZN5MXgY+DWC20hWJ/Wp9g5/g2zI01ae\nMemACnXSL6sx9LkqYB2N335I+VDk1u7Gnp+XMJDA4jMFy3IwQcffRqsMNsAzSsZ+\nuY4oI5Qcck0oDUjE8X7iwE+XeFoZsAy01UVVX4BmChTvI8TVFR1Mt2AABO4AMQli\nUQaaXGftlCPZnM3935WVGzJmV+YtBm6VykjrkUkwWYmuOLT0f6el6fBKIhkbMkEU\nEdlOxzoXQTDfTtDEjms4V1UrR6keG4akUqErpaBRMQKBgQDn0wMwVisjZUhLhqkO\n4RfhsS2dSUj4kIH9ZgB5KNDVKz0Iuf/eu9mhECnEWJOn3So3l1Ivmv7jrHxwooDJ\nTpWZZMgGwZWU8jXPqAetEs9j8JYRdEl6g7GGsF83bVfX1lCsoy57m4NKEDpTXoHK\nHQZ4bwVbVdz0rxB7PJodUUFNfwKBgQDWBdu9ynQnB7U1cNNgdMGvs/YhUFVmxA5+\nW3PBu3L0OUZdxf7PI0gWvO1O6xOo+epuf/BpDxflwDA7aEK6XgVsOzIouwyoCH7E\nqaeAg4HDixEVIquVevJeDBf+cuZWh6B2bifjYMNt4xSG5Fgl/GStKerBlJK8DJ2M\nkmidlV0Y0QKBgD0JQrKsTUlRdlpkRwii3S3P/2mFDRBcw8za2U8NHwaxDq3IgwS6\nws70OXr3s1CkG2Rtk/bywR9in5TsRCo8ZYjwjvWwbFi9+MsGu5am+DGg+3H3Aw2d\ntNT4hOlMAa/TPoRlpmxnAocyhJjErjlvsO9uk6N7lORbX2SDDTSg2yezAoGBAJfr\nYKe/5eMunXpptCFrv3CxFa1gEL75vDAFSo3iOR8S6Nf3i4ANU0aQpQK36ySvAbGe\nL1Dj3drAKXyB7ZvM0ZyaCGjRHZLJNPL0Wmnm712WgXG3QoTA2PnZK876UHZrn4Nx\nfe7w6Mg/wHpf7BJKrkwczP7ML7w8WDlIU+pH8+WBAoGAU/Cy+Sg130GHu3Y+boAj\nP0br7lRkoua9HvRzoINRy/UE8ClwTkm/640rEByvH2j00LvgOEDyO4kLPSRYXcCS\nX9/I5KOBYzoBpavorLLa8ozf4x3KR1NDichGZs+DNccx9wCOjoP9It19cfnTFEVr\nDdIjVW77wVNlyd4M4Or9nYc=\n-----END PRIVATE KEY-----\n",
-  "client_email": "resumatch@resumatch-277502.iam.gserviceaccount.com",
-  "client_id": "100951337452385806242",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/resumatch%40resumatch-277502.iam.gserviceaccount.com"
-}
+from googleapiclient.discovery import build
+from googleapiclient.errors import Error
+from google.auth.credentials import Credentials
 
-
-client = talent_v4beta1.JobServiceClient(credentials=GOOGLE_APPLICATION_CREDENTIALS)
-
-parent = client.project_path("resumatch")
 request_metadata = {
-    "before_request": 0
+    'domain':     'UNKNOWN',
+    'session_id': 'UNKNOWN',
+    'user_id':    'UNKNOWN',
 }
 
-for page in client.search_jobs(parent, request_metadata).pages:
-    print(page)
+os.environ["GOOGLE_CLOUD_PROJECT"] = "resumatch"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "resumatch-google-app-credentials.json"
+project_id = 'projects/' + os.environ['GOOGLE_CLOUD_PROJECT']
+
+# Build the service object, passing in the api name and api version
+client_service = build('jobs', 'v3')
+
+def create_company(request):
+    # This is the API call that actually creates the new company.
+    result = client_service.projects().companies().create(parent=project_id, body=request).execute()
+    return result
+
+
+def create_job(request):
+    # The actual API call happens here.
+    result = client_service.projects().jobs().create(parent=project_id, body=request).execute()
+    print('Job created: %s' % result)
+    return result
+
+def search_jobs(request):
+    
+    result = client_service.projects().jobs().search(parent=project_id, body=request).execute()
+    return result
+
+def search_software():
+    software_request = {
+        "request_metadata": request_metadata,
+        "jobQuery": {
+            "query": "software engineer"
+        }
+    }
+
+    search_jobs(software_request)
+
+search_software()
+
+
+
+
+
+def create_foo():
+    foocorp_company_request = {
+        "company": {
+            'display_name': "FooCorp",
+            'external_id': "foo2_llc"
+        }
+    }
+
+    result_company = create_company(foocorp_company_request)
+    company_name = result_company.get('name')
+
+    job = {
+        'company_name': company_name,
+        'title': 'Senior Software Engineer',
+        'addresses': ["Mountain View, CA"],
+
+        'description':
+        """Experienced software engineer required for full-time position.
+        Leadership ability and ability to thrive in highly competitive environment a must.
+        <p />Ignore postings from that "Bar" company, their microkitchen is terrible.  Join Team Foo!""",
+        'requisition_id': 'foo_swe',
+        'application_info': {
+            'uris': ['http://www.example.com/foo/software-engineer-application'],
+            'emails': ['apply@example.com']
+        }
+    }
+    request = {'job': job}
+    result_job = create_job(request)
+
+def create_horsehub():
+    horsehub_company_request = {
+        "company": {
+            'display_name': "Horse Hub",
+            'external_id': "horsies_llc"
+        }
+    }
+
+    result_company = create_company(horsehub_company_request)
+    company_name = result_company.get('name')
+
+    job = {
+        'company_name': company_name,
+        'title': 'Junior Software Engineer',
+        'description':
+        """Hiring entry level software engineer required for full-time position.
+        Must be passionate about industry intersection of horses and technology.
+        Ability to intelligently discuss the equine singularity a major bonus.
+        <p />C'mon bub! Join Horse Hub!""",
+        'requisition_id': 'hh_swe',
+        'application_info': {
+            'uris': ['http://www.example.com/foo/software-engineer-horsehub'],
+            'emails': ['apply-horsehub@example.com']
+        }
+    }
+    request = {'job': job}
+    result_job = create_job(request)
+
+def create_tandem():
+    tandem_company_request = {
+        "company": {
+            'display_name': "Tandem",
+            'external_id': "tandem"
+        }
+    }
+
+    result_company = create_company(tandem_company_request)
+    company_name = result_company.get('name')
+
+    job = {
+        'company_name': company_name,
+        'title': 'Test Engineer',
+        'description':
+        """Hiring Test Engineer for full-time position with Tandem.  Must be detail oriented
+        and (obviously) comfortable with pair programming.  Will be working with team of Software Engineers.
+        <p />Join Tandem today!""",
+        'requisition_id': 'tandem_te',
+        'application_info': {
+            'uris': ['http://www.example.com/tandem/test-engineer'],
+            'emails': ['apply-tandem-test@example.com']
+        },
+        'promotionValue': 1
+    }
+    request = {'job': job}
+    result_job = create_job(request)
+    job['requisition_id'] = 'a_tandem_te'
+    result_job = create_job(request)
+
+# try:
+#     create_foo()
+#     create_horsehub()
+#     create_tandem()
+
+# except Error as e:
+#     print('Got exception while creating company')
+#     raise e
